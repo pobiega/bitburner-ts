515aab8975c34f97463a3e0f31d8c5c169e72495
Robin Rexstedt <robin.rexstedt@stretch.se>
2021-12-20 10:31:46 +0100

    WIP: automate contract solver.

--- src/findContracts.ts
+++ src/findContracts.ts
@@ -1,14 +1,56 @@
 import { NS } from "../types/index.js";
 import { explore } from "./controller.js";
 
+const DEBUG = {
+    contractFound: true
+};
+
+type Solver = (data: any) => any;
+
+const solvers = {
+    "Algorithmic Stock Trader III": algoStockTrader3
+} as Record<string, Solver>;
+
 export async function main(ns: NS) {
     const servers = await explore(ns);
 
     for (const server of Object.values(servers)) {
         for (const file of server.files) {
             if (file.endsWith(".cct")) {
-                ns.tprint(`WARN: Contract ${file} located on ${server.host}.`);
+                if (DEBUG.contractFound)
+                    ns.tprint(`WARN: Contract ${file} located on ${server.host}.`);
+
+                const contractType = ns.codingcontract.getContractType(file, server.host);
+
+                const solver = solvers[contractType];
+
+                if (solver !== undefined) {
+                    const data = ns.codingcontract.getData(file, server.host);
+                    const answer = solver(data);
+
+                    if (answer !== undefined) {
+                        const reward = ns.codingcontract.attempt(answer, file, server.host, { returnReward: true });
+
+                        ns.tprint(`INFO: ${file} on ${server.host} solved! Reward: ${reward}`);
+                    }
+                }
             }
         }
     }
+}
+
+function algoStockTrader3(numbers: number[]) {
+    let hold1: number = Number.MIN_SAFE_INTEGER;
+    let hold2: number = Number.MIN_SAFE_INTEGER;
+    let release1 = 0;
+    let release2 = 0;
+
+    for (const price of numbers) {
+        release2 = Math.max(release2, hold2 + price);
+        hold2 = Math.max(hold2, release1 - price);
+        release1 = Math.max(release1, hold1 + price);
+        hold1 = Math.max(hold1, price * -1);
+    }
+
+    return release2;
 }
